066b12fb4864f76b8d5c2ab00537ec227e7a561e
Rob Mathers <robmathers@gmail.com>
2012-08-09 14:37:01 -0400

    Imported CoreLocation Framework

--- WhereAmI/main.m
+++ WhereAmI/main.m
@@ -11,12 +11,9 @@
 int main(int argc, const char * argv[])
 {
 
-    @autoreleasepool {
+    // insert code here...
+    NSLog(@"Hello, World!");
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
-    }
     return 0;
 }
 
