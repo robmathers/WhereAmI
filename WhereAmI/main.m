4df398d5d1334b11c1a0e4b5139dcf1faa8047a5
Rob Mathers <robmathers@gmail.com>
2012-08-09 14:02:57 -0400

    Initial Commit

---
+++ WhereAmI/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WhereAmI
+//
+//  Created by Rob Mathers on 12-08-09.
+//  Copyright (c) 2012 Rob Mathers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
